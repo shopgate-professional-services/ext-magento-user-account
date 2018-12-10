314b323e78da6f4925a94b800511c13c6db121d1
Alexander Wesselburg <info@myportal.de>
2018-12-10 13:49:50 +0100

    MAGENTO-2299 - added portal for show OrderHistory link

--- frontend/components/OrderHistory/index.jsx
+++ frontend/components/OrderHistory/index.jsx
@@ -1,10 +1,10 @@
-import PipelineRequest from '@shopgate/pwa-core/classes/PipelineRequest'
-import React from 'react'
-import PropTypes from 'prop-types'
-import I18n from '@shopgate/pwa-common/components/I18n'
-import Icon from '@shopgate/pwa-ui-shared/icons/BoxIcon'
-import connect from '../../connector'
-import { showOrderHistory } from '../../config'
+import PipelineRequest from '@shopgate/pwa-core/classes/PipelineRequest';
+import React from 'react';
+import PropTypes from 'prop-types';
+import I18n from '@shopgate/pwa-common/components/I18n';
+import Icon from '@shopgate/pwa-ui-shared/icons/BoxIcon';
+import connect from '../../connector';
+import { showOrderHistory } from '../../config';
 
 /**
  * OrderHistory
@@ -12,48 +12,65 @@ import { showOrderHistory } from '../../config'
  * @returns {JSX}
  */
 class OrderHistory extends React.Component {
-
-  constructor (props) {
-    super(props)
+  /**
+     * The constructor.
+     * Sets the initial state from connected props.
+     * @param {Object} props The component props.
+     */
+  constructor(props) {
+    super(props);
     this.state = {
       url: null,
-    }
+    };
   }
 
-  componentDidMount () {
+  /**
+   * Constructor
+   */
+  componentDidMount() {
     if (this.props.isUserLoggedIn) {
-      this.fetchUrl()
+      this.fetchUrl();
     }
   }
 
-  componentDidUpdate (prevProps) {
+  /**
+   * @param {Object} prevProps prevProps
+   */
+  componentDidUpdate(prevProps) {
     if (!prevProps.isUserLoggedIn && this.props.isUserLoggedIn) {
-      this.fetchUrl()
+      this.fetchUrl();
     }
   }
 
-  fetchUrl () {
+  /**
+   * Fetch the order history url and set the state
+   */
+  fetchUrl() {
     if (!this.props.show || this.state.url) {
-      return
+      return;
     }
 
     new PipelineRequest('shopgate.checkout.getUrl').dispatch().then((response) => {
       if (response.url) {
-        this.setState({ url: response.url + '?module=sales&controller=order&action=history' })
+        this.setState({ url: `${response.url}module/sales/controller/order/action/history` });
       }
-    })
+    });
   }
 
-  render () {
+  /**
+   * The render method
+   * @return {JSX}
+   */
+  render() {
     const {
       isUserLoggedIn,
       Item,
       handleClose,
       show,
-    } = this.props
+    } = this.props;
 
     if (!showOrderHistory || !show || !isUserLoggedIn || !this.state.url) {
-      return null
+      return null;
     }
 
     return (
@@ -63,9 +80,9 @@ class OrderHistory extends React.Component {
         title="navigation.my_orders"
         icon={Icon}
       >
-        <I18n.Text string="navigation.my_orders"/>
+        <I18n.Text string="navigation.my_orders" />
       </Item>
-    )
+    );
   }
 }
 
@@ -74,11 +91,11 @@ OrderHistory.propTypes = {
   Item: PropTypes.func.isRequired,
   handleClose: PropTypes.func,
   show: PropTypes.bool,
-}
+};
 
 OrderHistory.defaultProps = {
-  handleClose: () => {},
-  show: true,
-}
+  handleClose: () => { },
+  show: false,
+};
 
-export default connect(OrderHistory)
+export default connect(OrderHistory);
---
+++ frontend/portals/OrderHistory/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import OrderHistory from '../../components/OrderHistory';
+
+export default props => <OrderHistory {...props} show />;
